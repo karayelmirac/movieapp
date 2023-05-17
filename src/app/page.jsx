c8dd484da170eee97f174f5e00aa6f89b767bca2
Mirac Karayel <mirackarayel07@gmail.com>
2023-05-18 01:33:03 +0300

    MovieApp With Nextjs and TailwindCSS

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+const Page = async({searchParams}) => {
+  console.log(searchParams.genre,"searchParams")
+
+
+  //fetch data for api
+  //const res = await fetch()
+  return (
+    <div>
+      Veri cekme alani
+    </div>
+  );
+};
+
+export default Page;
